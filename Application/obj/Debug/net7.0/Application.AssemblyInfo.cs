6b190b40578243f0b5a6bcad78711b1944ff36d6
M2ri <SergiGullam@gmail.com>
2023-11-26 09:42:40 +0200

    enhane gitignire

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be55d7362cd7393121e8e4f4c57165656a04b4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438ac6d55ff28e004239ad53dff9043483079e0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be55d7362cd7393121e8e4f4c57165656a04b4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438ac6d55ff28e004239ad53dff9043483079e0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
