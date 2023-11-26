1c35984de2a0c937b801016776b054850d99f6c4
M2ri <SergiGullam@gmail.com>
2023-11-26 09:44:29 +0200

    **/obj/*

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438ac6d55ff28e004239ad53dff9043483079e0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b190b40578243f0b5a6bcad78711b1944ff36d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
