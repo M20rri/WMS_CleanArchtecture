e0a99b9e62ea65d95deffa7cff9a0be776c20d01
M2ri <SergiGullam@gmail.com>
2023-11-27 07:11:30 +0200

    Add

--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eab9c3469a8f509dee278e77771081cda4671da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3dc56b10a5350ede55ee49335d76f2ab40a3d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eab9c3469a8f509dee278e77771081cda4671da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3dc56b10a5350ede55ee49335d76f2ab40a3d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
