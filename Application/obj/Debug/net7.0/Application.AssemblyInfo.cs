bacf46104770a25729864293fe481912969d834b
M2ri <SergiGullam@gmail.com>
2023-11-27 07:15:20 +0200

    ignore bin and obj folders

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a99b9e62ea65d95deffa7cff9a0be776c20d01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68e04a8dcb0abce6d1f6a4cfe751845169e9ad9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
